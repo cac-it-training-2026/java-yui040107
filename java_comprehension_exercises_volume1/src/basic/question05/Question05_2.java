3c651863aade1e59829b4706e2a880c330a78294
yuioha <yuioha@C010423033.zipangu.cac.co.jp>
2026-04-16 13:30:50 +0900

    Question05まで完了

--- java_comprehension_exercises_volume1/src/basic/question05/Question05_2.java
+++ java_comprehension_exercises_volume1/src/basic/question05/Question05_2.java
@@ -10,7 +10,19 @@ public class Question05_2 {
 
 	public static void main(String[] args) throws IOException {
 
-		// TODO:ここに実装
+		int[] numberGroup = { 3, 5, 7, 9 };
+
+		if (numberGroup[3] == 3) {
+			System.out.println("4番目の要素の値は３です。");
+
+		} else if (numberGroup[3] == 5) {
+			System.out.println("4番目の要素の値は５です。");
+		} else if (numberGroup[3] == 7) {
+			System.out.println("4番目の要素の値は7です。");
+		} else {
+			System.out.println("4番目の要素の値は3，5，7以外の値です。");
+		}
 
 	}
+
 }
